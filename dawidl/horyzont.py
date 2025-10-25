7d3970b93fee7eb9981b4e39d3a6b1751442a969
DawidLukasik13 <245463@student.uek.krakow.pl>
2025-10-25 10:13:17 +0200

    Add files via upload

---
+++ dawidl/horyzont.py
@@ -0,0 +1,7 @@
+##Horizonte
+import math
+
+h = float(input ('wysokosc: '))
+d = 3,57 * math.sqrt(h)
+
+print('Odleglosc do horyzontu wynosi: ', d)
---
+++ dawidl/przekatna.py
@@ -0,0 +1,17 @@
+###
+# A program that calculates the length of the diagonal
+# of a rectangle with sides a and b.
+#
+import math
+a = 5
+b = 8
+diagonal = math.sqrt(a**2 + b**2) 
+print(diagonal)
+
+
+##Horizonte
+h = float(input ('wysokosc: '))
+h1 = math.sqrt(h)
+d = 3,57 * h1
+
+print('Odleglosc do horyzontu wynosi: ', d)
